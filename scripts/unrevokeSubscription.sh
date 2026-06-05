3a8ca9cd2f477e92f7d28aa9e8beab54e041fda7
Robel Getnet Geremew <robelgetnetrg@gmail.com>
2026-06-05 11:11:16 +0300

    Add gas scaling benchmarks and CI (#140)
    * Tests working
    
    * docs for gas tests
    
    * CI and Gas chart generation
    
    * ci-test:0
    
    * ci-test:1
    
    * ci-test:2
    
    * ci-test:3
    - force PR
    
    * ci-test:4
    - reverted test triggers for foundry-gas.yml
    - force PR on gas-benchmarks.yml
    
    * ci-test:5
    
    * ci-test:6
    
    * reverted test triggers and added snapshot files
    
    * revoke and unrevoke scripts
    
    * docs for new scripts

---
+++ scripts/unrevokeSubscription.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+source ./scripts/utils.sh
+
+source_environment $1
+if [ $? -eq 0 ]; then
+    shift 1
+fi
+
+registry_index=$1
+asset_id=$(cast keccak "$2")
+subscriber=$3
+asset_owner_private_key=$4
+
+registry_address=$(get_address $registry_index)
+
+asset_address=$(cast call $registry_address "getAsset(bytes32)(address)" $asset_id --rpc-url $RPC_URL --from $(get_wallet_address 0))
+
+result=$(cast send $asset_address "unrevokeSubscription(bytes32)" $subscriber --rpc-url $RPC_URL --private-key $asset_owner_private_key --json)
+EXIT_CODE=$?
+cooldown
+
+if [ $EXIT_CODE -ne 0 ]; then
+    return $EXIT_CODE 2>/dev/null || exit $EXIT_CODE
+fi
+
+transaction_hash=$(echo $result | jq -r '.transactionHash')
+
+revoked=$(cast call $asset_address "isSubscriberRevoked(bytes32)(bool)" $subscriber --rpc-url $RPC_URL --from $(get_wallet_address 0) --json | jq -r '.[0]')
+
+echo "Asset Address: $asset_address
+Asset ID: $2
+Subscriber: $subscriber
+Revoked: $revoked
+Transaction Hash: $transaction_hash"
